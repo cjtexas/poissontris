0f674bf1d3dd9961f78db2c71f5fa514caca2d3d
Linards Kalvāns <linarrds.kalvans@twino.eu>
2019-02-20 14:46:58 +0200

    Try to fix deployment

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-chmod 600 id_rsa;
+chmod 600 id_rsa
 scp -o StrictHostKeyChecking=no -i id_rsa docker-compose.yml ubuntu@poissontris.linards.net:~/
-if [ "$TRAVIS_BRANCH" == "master" ]; then SERVICE=poissontris; SERVICE=poissontris-"$TRAVIS_BRANCH"; fi
-ssh -o StrictHostKeyChecking=no -i id_rsa ubuntu@poissontris.linards.net "docker-compose down; docker-compose -d up $SERVICE"
+ssh -o StrictHostKeyChecking=no -i id_rsa ubuntu@poissontris.linards.net "docker-compose down; docker-compose -d up $1"
