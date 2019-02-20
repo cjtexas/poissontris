5945cd8901960bdf0825d0dae2e8709fa43cd74e
Linards Kalvāns <linarrds.kalvans@twino.eu>
2019-02-20 15:08:58 +0200

    Fix docker-compose args

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 chmod 600 id_rsa
 scp -o StrictHostKeyChecking=no -i id_rsa docker-compose.yml ubuntu@poissontris.linards.net:~/
-ssh -o StrictHostKeyChecking=no -i id_rsa ubuntu@poissontris.linards.net "docker-compose down; docker-compose up -d $1"
+ssh -o StrictHostKeyChecking=no -i id_rsa ubuntu@poissontris.linards.net "docker-compose down --remove-orphans; docker-compose up -d $1"
