2db04993406c62a8d9b6252548142fbb958d7a39
Linards Kalvans <linards.kalvans@gmail.com>
2019-02-20 21:22:21 +0200

    Force stopping docker compose services

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 chmod 600 id_rsa
 scp -o StrictHostKeyChecking=no -i id_rsa docker-compose.yml ubuntu@poissontris.linards.net:~/
-ssh -o StrictHostKeyChecking=no -i id_rsa ubuntu@poissontris.linards.net "docker-compose rm -s $1; docker-compose pull $1; docker-compose up -d $1"
+ssh -o StrictHostKeyChecking=no -i id_rsa ubuntu@poissontris.linards.net "docker-compose rm -fs $1; docker-compose pull $1; docker-compose up -d $1"
