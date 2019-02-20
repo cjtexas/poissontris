3d62416291fa484146dc540fe166014a62fbc011
Linards Kalvāns <linarrds.kalvans@twino.eu>
2019-02-20 14:51:28 +0200

    Debug deployment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+pwd
 chmod 600 id_rsa
 scp -o StrictHostKeyChecking=no -i id_rsa docker-compose.yml ubuntu@poissontris.linards.net:~/
 ssh -o StrictHostKeyChecking=no -i id_rsa ubuntu@poissontris.linards.net "docker-compose down; docker-compose -d up $1"
