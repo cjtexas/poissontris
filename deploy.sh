64398a24f1b763044a0ff86ffe72b25304f1686f
Linards Kalvans <linards.kalvans@gmail.com>
2019-02-23 21:03:00 +0200

    Hide deployment host / user

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 chmod 600 id_rsa
-scp -o StrictHostKeyChecking=no -i id_rsa docker-compose.yml ubuntu@poissontris.linards.net:~/
-ssh -o StrictHostKeyChecking=no -i id_rsa ubuntu@poissontris.linards.net "docker-compose rm -fs $1; docker-compose pull $1; docker-compose up -d $1"
+scp -o StrictHostKeyChecking=no -i id_rsa docker-compose.yml $1:~/
+ssh -o StrictHostKeyChecking=no -i id_rsa $1 "docker-compose rm -fs $2; docker-compose pull $2; docker-compose up -d $2"
