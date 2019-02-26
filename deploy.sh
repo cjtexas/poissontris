57fbb90b5ab62d2a5c4f8febdf925984efbef8bd
Linards Kalvans <linards.kalvans@gmail.com>
2019-02-26 22:00:20 +0200

    Set working directory in docker-compose

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 chmod 600 id_rsa
 scp -o StrictHostKeyChecking=no -i id_rsa docker-compose.yml $1:$3
-ssh -o StrictHostKeyChecking=no -i id_rsa $1 "printenv; cd $3; docker-compose stop $2; docker-compose pull $2; docker-compose up -d $2"
+ssh -o StrictHostKeyChecking=no -i id_rsa $1 "cd $3; docker-compose stop $2; docker-compose pull $2; docker-compose up -d $2"
