a9942948a6ed47bffbf6670643d8c3d7638d8bf3
Linards Kalvans <linards.kalvans@gmail.com>
2019-02-26 20:28:59 +0200

    Deploy with nginx-proxy + letsencrypt

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 chmod 600 id_rsa
 scp -o StrictHostKeyChecking=no -i id_rsa docker-compose.yml $1:~/
-ssh -o StrictHostKeyChecking=no -i id_rsa $1 "docker-compose rm -fs $2; docker-compose pull $2; docker-compose up -d $2"
+ssh -o StrictHostKeyChecking=no -i id_rsa $1 "docker-compose stop $2; docker-compose pull $2; docker-compose up -d $2"
